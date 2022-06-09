544e8335129a8ebe4d9a9d57b97e8fdaab83f8c7
ChandanaShivakumar <chandanashiva55@gmail.com>
2022-06-09 21:05:29 +0530

    Add files via upload

---
+++ SprintMVC/wwwroot/js/site.js
@@ -0,0 +1,26 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+//modified by me
+
+function myFunction() {
+    var x = document.getElementById("Password");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
+
+$('#toggle').click(function () {
+    //check if checkbox is checked
+    if ($(this).is(':checked')) {
+
+        $('#sendNewSms').removeAttr('disabled'); //enable input
+
+    } else {
+        $('#sendNewSms').attr('disabled', true); //disable input
+    }
+});
