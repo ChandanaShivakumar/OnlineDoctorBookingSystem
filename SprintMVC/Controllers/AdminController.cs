71588491bf3e8464e41254d22c1c02da8ffb3050
ChandanaShivakumar <chandanashiva55@gmail.com>
2022-06-09 21:00:17 +0530

    Add files via upload

---
+++ SprintMVC/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SprintMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Schedule()
+        {
+            return View();
+        }
+
+        public IActionResult Patient()
+        {
+            return View();
+        }
+
+        public IActionResult Appointment()
+        {
+            return View();
+        }
+
+        public IActionResult Mainn()
+        {
+            return View();
+        }
+        public IActionResult Messages()
+        {
+            return View();
+        }
+    }
+}
---
+++ SprintMVC/Controllers/UserController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SprintMVC.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Under progress
+            return View();
+        }
+        public IActionResult Login()
+        {
+            //Login
+            return View();
+        }
+        public IActionResult Register()
+        {
+            //Create and register User
+            return View();
+        }
+        public IActionResult DoctorList()
+        {
+            //View DoctorList
+            return View();
+        }
+
+        public IActionResult DoctorScheduleList()
+        {
+            //View DoctorScheduleList
+            return View();
+        }
+
+        public IActionResult BookAppointment()
+        {
+            //Book Appointment
+            return View();
+        }
+
+        public IActionResult Patient()
+        {
+            //Patient Details
+            return View();
+        }
+
+        public IActionResult Mainn()
+        {
+            //User Menu
+            return View();
+        }
+        public IActionResult About()
+        {
+            //About Us
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            //Contact Us
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            //Privacy
+            return View();
+        }
+        public IActionResult Testimonial()
+        {
+            //Testimonials
+            return View();
+        }
+    }
+}
